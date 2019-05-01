0c58ff97ff03b8aa1dc6ee248bda750d2bbd3e8c
teealberta <tirza.alberta@nyu.edu>
2019-05-01 21:40:33 +0700

    Updates

---
+++ precache-manifest.66c123cc43a2582f98ea2b98d355e351.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372a6a7ca4952272b0036e78e8ad4a0b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7099948f8865b21e3534",
+    "url": "/portfolio/static/css/main.8f7178e9.chunk.css"
+  },
+  {
+    "revision": "ff7a8cc4bbdae7b772c9",
+    "url": "/portfolio/static/js/2.5537876b.chunk.js"
+  },
+  {
+    "revision": "7099948f8865b21e3534",
+    "url": "/portfolio/static/js/main.ad276cd0.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
